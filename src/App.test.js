e7f9a0fae5a99090a84e8b3f5394cfad3c820109
Benjamin <blamboy090@gmail.com>
2022-10-24 20:14:27 -0600

    added tests for being able to type in input fields

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,6 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
 import App from "./App";
 
 // 1) rendering the component we want to test
@@ -14,4 +16,29 @@ test("inputs should be initially empty", () => {
   expect(emailInputElement.value).toBe("");
   expect(passwordInputElement.value).toBe("");
   expect(confirmPasswordInputElement.value).toBe("");
+});
+
+test("should be able to type an email", () => {
+  render(<App />);
+  const emailInputElement = screen.getByRole("textbox", { name: /email/i });
+  userEvent.type(emailInputElement, "annabelle@cake.com");
+  expect(emailInputElement.value).toBe("annabelle@cake.com");
+});
+
+test("shoulb be able to type a password", () => {
+  render(<App />);
+  const passwordInputElement = screen.getByRole("textbox", {
+    name: /password/i,
+  });
+  userEvent.type(passwordInputElement, "ABC123");
+  expect(passwordInputElement.value).toBe("ABC123");
+});
+
+test("shoulb be able to type a confirmation password", () => {
+  render(<App />);
+  const confirmPasswordInputElement = screen.getByLabelText("Confirm Password", {
+    name: /confirm-password/i,
+  });
+  userEvent.type(confirmPasswordInputElement, "ABC123");
+  expect(confirmPasswordInputElement.value).toBe("ABC123");
 });
